ffabb98a4e38741cc9fe7c00147e4306e6fc6ec5
Michael Maliniak <mike@maliniak.io>
2020-01-31 19:42:43 +1100

    added scroll handling within app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Profile goes here...
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      position : 0
+    }
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+
+    let y = window.scrollY
+
+    this.setState({
+      position: y
+    })
+  }
+
+  render () {
+    return (
+      <div className="App" onScroll={this.update}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p style={{ fontSize: this.state.position * 0.1 }}>
+            Profile goes here...
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
