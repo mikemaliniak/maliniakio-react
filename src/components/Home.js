71cabbedd101983723fbef099bca9b2b80614d08
Michael Maliniak <mike@maliniak.io>
2020-02-24 07:35:34 +1100

    added scss breakpoint variables and mixin capability" git push

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,17 +12,17 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <section className="home">
+                <section className="home l-wrapper">
                     header
                 </section>
                 <section>
                     <div className="l__container">
-                        <div className="l__container__column">
+                        <div className="l__col">
                             <h2>About me</h2>
                             <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like on your page. </p>
                             <p>This is a great space to write long text about your company and your services. You can use this space to go into a little more detail about your company. Talk about your team and what services you provide.</p>
                         </div>
-                        <div className="l__container__column">
+                        <div className="l__col">
                             <img src={logo} alt=""/>
                         </div>
                     </div>
