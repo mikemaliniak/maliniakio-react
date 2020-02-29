df6d0cbd8dd38eec59d888887cbedda7cbe03fba
Michael Maliniak <mike@maliniak.io>
2020-02-29 17:10:11 +1100

    updated ProjectCard component to be able to link to specific projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ProjectCard = ({ name }) => {
     return (
-        <h1>{name}</h1>
+        <Link to={`/project/${name}`}>
+            <h1>{name}</h1>
+        </Link>
     )
 }
 
