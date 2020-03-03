b455d50910365222e0628890c7876187e1cb04a3
Michael Maliniak <mike@maliniak.io>
2020-03-04 07:35:03 +1100

    returned nvabar to original state due to too much bloat from mui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,26 @@
-import React, { Fragment } from 'react';
-import { NavLink, Link }from 'react-router-dom';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Slide from '@material-ui/core/Slide';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function HideOnScroll(props) {
-  const { children } = props;
-  // Don't need to set the window ref as useScrollTrigger
-  // will default to window.
-  const trigger = useScrollTrigger();
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-HideOnScroll.propTypes = {
-  children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
-
-export default function HideAppBar(props) {
-  return (
-    <Fragment>
-      <CssBaseline />
-      <HideOnScroll {...props}>
-        <AppBar>
-          <Toolbar>
-            <Typography variant="h6">
-                <Link to='/'>Maliniak.io</Link>
-            </Typography>
+class Navbar extends Component {
+    render() {
+        return (
             <nav>
-                <NavLink to="/projects">PROJECTS</NavLink>
-                <NavLink to="/skills">SKILLS</NavLink>
+                <NavLink to="/projects" activeClassName="is-active">Projects</NavLink>
+                <NavLink to="/skills" activeClassName="is-active">Skills</NavLink>
             </nav>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Toolbar />
-    </Fragment>
-  );
+        );
+    }
 }
+
+function mapStateToProps(state) {
+    return {
+
+    };
+}
+
+export default connect(
+    mapStateToProps,
+)(Navbar);
+
+
