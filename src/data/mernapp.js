73b53d49458cbdbba072e309875c981cc5a00aea
Michael Maliniak <mike@maliniak.io>
2021-06-02 17:45:56 +1000

    added more placeholder descriptions and tags

--- src/data/mernapp.js
+++ src/data/mernapp.js
@@ -2,5 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const mernapp = {
     id: uuidv4(),
-    name: 'mernapp'
+    name: 'mernapp',
+    tags: ['Non-Commercial', 'Node', 'React', 'Redux', 'Fullstack'],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in mauris ex. Aliquam ac augue et risus posuere consectetur. Curabitur et augue porta, pharetra sem quis, suscipit tortor. Mauris ultrices sed neque ut congue. Nam ut eros commodo, elementum ex eget, porta augue. Praesent rhoncus nibh vitae arcu elementum, non faucibus dui tincidunt. Cras varius felis at orci lobortis rutrum. Vivamus augue sem, consequat non leo et, malesuada commodo odio.\n Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent pellentesque elementum blandit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla ut sodales mauris, in ornare purus. Suspendisse nec ante accumsan, dictum massa vel, porta quam.'
 }
