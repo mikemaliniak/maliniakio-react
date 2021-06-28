6a583311f6c1938bb3ec2d6b4c15164fa80eab25
Michael Maliniak <mike@maliniak.io>
2021-06-28 19:51:56 +1000

    fixed formatting bug in amgen write-up

--- src/data/amgen.js
+++ src/data/amgen.js
@@ -5,5 +5,5 @@ export const amgen = {
     name: 'amgen',
     title: 'Osteoporosis - Vue App',
     tags: ['Node', 'Vue', 'Vuex', 'Webpack', 'Commercial'],
-    description: 'Great project that involved building a single-page application using the Vue.JS javascript framework./n  Working entirely remotely, was able to convert designs and project specs to build the dynamic SPA.  Built utilizing NodeJS, Webpack, Vue.JS, Vuex, SCSS as well as third-party vendor libaries.  Really enjoyable to build and has enhanced my interest in building using SPA frameworks.'
+    description: 'Special mentiron from my contracting work as this really was a great project that involved building a single-page application using the Vue.JS javascript framework.\n Working entirely remotely, was able to convert designs and project specs to build the dynamic SPA.  Built utilizing NodeJS, Webpack, Vue.JS, Vuex, SCSS as well as third-party vendor libaries.  Really enjoyable to build and has enhanced my interest in building using SPA frameworks.'
 }
