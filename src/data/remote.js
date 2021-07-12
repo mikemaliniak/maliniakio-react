fc8a4d5b87195fe9eb5cca9011103fd76ad4d4bb
Michael Maliniak <mike@maliniak.io>
2021-07-12 20:05:47 +1000

    added content for remote work

--- src/data/remote.js
+++ src/data/remote.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const remoteContracting = {
     id: uuidv4(),
     name: 'remote',
-    title: 'Remote Contracting',
-    tags: ['Commercial', 'Wordpress', 'Vue', 'Vuex', 'CMS', 'SiteCore'],
-    description: 'Projects large and small'
+    title: 'Contract Developer - Remote + On Site',
+    tags: ['Commercial', 'Wordpress', 'Vue', 'Vuex', 'CMS', 'SiteCore', 'PHP', 'Node'],
+    description: 'Due to establishing strong relationships and confidence in my skillset and work ethic, I was able to focus solely on contract work, both onsite and fully remote for the best part of 15 months. Projects, clients, programming languages and tech-stacks were varied ranging in the form of Wordpress, Sitecore, PHP, Node and Vue.js to name but a few.\n Both rewarding and challenging in that as a contractor the project must be completed on time, with only your own skillset and perseverance to rely on.'
 }
